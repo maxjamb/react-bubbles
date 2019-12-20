709b3f278102ce27b90076f6421aa4e20216b4a6
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-12-20 09:30:58 +0000

    colorlist and bubbles mounted with auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+import { PrivateRoute } from "./components/privateRoute";
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute exact path="/colors" component={BubblePage} />
       </div>
     </Router>
   );
--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,11 +3,24 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "./axios/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("http://localhost:5000/api/colors")
+      .then(res => {
+        setColorList(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+        alert("couldnt get colors");
+      });
+  }, []);
 
   return (
     <>
